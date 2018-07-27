7022160406dc93c90d83e40c3854ecdfbea6a1ee
Egomancer <agireada@newrosoft.com>
2018-07-27 15:46:33 +0300

    First version

---
+++ Assets/Scripts/UI_Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Controller : MonoBehaviour {
+
+	[SerializeField] Animator canvasAnimator;
+
+	public void SwitchViews() {
+		canvasAnimator.SetTrigger ("SwitchViews");
+	}
+}
