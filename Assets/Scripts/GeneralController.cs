1a2bad65d66c9efde0d0df0fa4e55b4d53babefe
Egomancer <agireada@newrosoft.com>
2018-07-30 15:27:33 +0300

    Updated version: - Split timer text into three separate ones for hours, minutes, seconds, removing string concatenation. - Used separate scripts for cube spawning and timer. - Changed timer text updating from every frame to only when something has changed. - Switched from cube destroying to reusing old ones.

--- Assets/Scripts/GeneralController.cs
+++ Assets/Scripts/GeneralController.cs
@@ -1,38 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GeneralController : MonoBehaviour {
-
-	[SerializeField] Text timerText;
-
 	[SerializeField] GameObject cubePrefab;
 	System.DateTime time;
+	System.DateTime lastTime;
 	int lastSpawnTime;
 
 	List<GameObject> cubeList;
-
 	Vector3 spawnPos;
-
 	int oldestCubeIndex;
 
 	// Use this for initialization
 	void Start () {
-		time = System.DateTime.UtcNow;
 		lastSpawnTime = -1;
 		spawnPos = new Vector3 (-6f, 0, 0);
 		cubeList = new List<GameObject> ();
-	}
-
-	void SetTimer() {
-		timerText.text = time.Hour + ":" + time.Minute + ":" + time.Second;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		time = System.DateTime.UtcNow;
-		SetTimer ();
 
 		if (time.Second % 2 == 0 && time.Second != lastSpawnTime) {
 			SpawnCube ();
@@ -42,11 +31,7 @@ public class GeneralController : MonoBehaviour {
 
 	void SpawnCube() {
 		if (cubeList.Count == 10) {
-			Destroy (cubeList [oldestCubeIndex]); //REMOVE OLDEST CUBE
-			spawnPos.x = -6f + oldestCubeIndex * 1.1f;
-			GameObject v_cube = GameObject.Instantiate (cubePrefab, spawnPos, Quaternion.identity);
-			v_cube.transform.localScale = Vector3.one * Random.Range (0.3f, 1f); //SET RANDOM SCALE JUST TO DIFFERENTIATE THE NEW CUBE FROM THE OLD CUBE
-			cubeList [oldestCubeIndex] = v_cube;
+			cubeList[oldestCubeIndex].transform.localScale = Vector3.one * Random.Range (0.3f, 1f);
 			oldestCubeIndex = (oldestCubeIndex + 1) % 10;
 		} else {
 			spawnPos.x = -6f + cubeList.Count * 1.1f;
---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour {
+	
+	[SerializeField] Text timerHoursText;
+	[SerializeField] Text timerMinutesText;
+	[SerializeField] Text timerSecondsText;
+
+	System.DateTime time;
+	System.DateTime lastTime;
+
+	// Use this for initialization
+	void Start () {
+		time = lastTime = System.DateTime.UtcNow;
+
+		timerHoursText.text = time.Hour.ToString("00");
+		timerMinutesText.text = time.Minute.ToString("00");
+		timerSecondsText.text = time.Second.ToString("00");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		time = System.DateTime.UtcNow;
+		SetTimer ();
+	}
+
+	void SetTimer() {
+		if(time.Hour != lastTime.Hour) {
+			timerHoursText.text = time.Hour.ToString("00");
+		}
+
+		if(time.Minute != lastTime.Minute) {
+			timerMinutesText.text = time.Minute.ToString("00");
+		}
+
+		if(time.Second != lastTime.Second) {
+			timerSecondsText.text = time.Second.ToString("00");
+		}
+	}
+}
